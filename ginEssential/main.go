dfc984a6468ae867b8d021140e20b86f9d1f9618
Arron2411616261 <15018135093@163.com>
2023-02-27 23:54:46 +0800

    second commit

--- ginEssential/main.go
+++ ginEssential/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
 )
 
+type User struct {
+	gorm.Model
+	Name      string
+	Telephone string
+	Password  string
+}
+
 func main() {
+	db := InitDB()
+	//defer db.Close()
 	r := gin.Default()
 	r.POST("/api/auth/register", func(ctx *gin.Context) {
 		name := ctx.PostForm("name")
@@ -26,6 +38,16 @@ func main() {
 		if len(name) == 0 {
 			name = randomString(10)
 		}
+		if isTelephoneExist(db, telephone) {
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "exists"})
+		}
+
+		newUser := User{
+			Name:      name,
+			Telephone: telephone,
+			Password:  password,
+		}
+		db.Create(&newUser)
 		log.Println(name, telephone, password)
 		ctx.JSON(200, gin.H{
 			"msg": "register successfully",
@@ -34,13 +56,45 @@ func main() {
 	r.Run()
 }
 
+func isTelephoneExist(db *gorm.DB, telephone string) bool {
+	var user User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+	return false
+}
+
 func randomString(n int) string {
 	var letters = []byte("wqyeoriodndjahngklJGIGSFBGFWMNBKSBGMBWIOQPSZVNXMZScxzmhoiewpqjfdnavgcz")
 	result := make([]byte, n)
 
-	rand.Seed(time.Now().Unix())
+	rand.Seed(time.Now().UnixNano())
 	for i := range result {
 		result[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(result)
+}
+
+func InitDB() *gorm.DB {
+	//driverName := "mysql"
+	host := "localhost"
+	port := "3306"
+	database := "ginessential"
+	username := "root"
+	password := "root"
+	charset := "utf8"
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset)
+
+	db, err := gorm.Open(sqlite.Open(args), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database, err: " + err.Error())
+	}
+	return db
 }
