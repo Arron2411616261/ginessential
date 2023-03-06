57b34c57d7b503cf2e9b6cd0baf5a62b96ddc09a
Arron2411616261 <15018135093@163.com>
2023-03-06 19:17:23 +0800

    登录后端

--- ginEssential/controller/UserController.go
+++ ginEssential/controller/UserController.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"log"
 	"net/http"
@@ -32,17 +33,59 @@ func Register(ctx *gin.Context) {
 		return
 	}
 
+	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "hase err"})
+		return
+	}
 	newUser := model.User{
 		Name:      name,
 		Telephone: telephone,
-		Password:  password,
+		Password:  string(hasedPassword),
 	}
 	DB.Create(&newUser)
 
 	//返回结果
 	log.Println(name, telephone, password)
 	ctx.JSON(200, gin.H{
-		"msg": "register successfully",
+		"code": 200,
+		"msg":  "register successfully",
+	})
+}
+
+func Login(ctx *gin.Context) {
+	DB := common.GetDB()
+
+	telephone := ctx.PostForm("telephone")
+	password := ctx.PostForm("password")
+
+	if len(telephone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "the length of the telephone must be 11"})
+		return
+	}
+	if len(password) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "the length of the password must be more than 6"})
+		return
+	}
+
+	var user model.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "this user doesn't exist"})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "password error"})
+		return
+	}
+
+	token := "111"
+
+	ctx.JSON(200, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "login in successfully",
 	})
 }
 
